46692492b14f08f5156a2ee36d706ef10d0a23c2
kevbite <kev_bite@msn.com>
2015-12-16 22:30:55 +0000

    really shit twitter stuff... cos their api is now crap.

--- js/site.js
+++ js/site.js
@@ -22,4 +22,17 @@ $(document).ready(function() {
 	    }, 900, 'swing');
 	});
 
+  	var quotes = $(".quotes");
+  	var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
 });
